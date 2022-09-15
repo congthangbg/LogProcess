4ef4fde6f68f19104c09289ac260bce2365c5655
thangdc <you@example.com>
2022-09-15 09:27:32 +0700

    teee

--- ghi-log-process-win10/src/main/java/com/vn/controller/FileResourcesController.java
+++ ghi-log-process-win10/src/main/java/com/vn/controller/FileResourcesController.java
@@ -56,7 +56,7 @@ import com.vn.common.sendData;
 public class FileResourcesController {
 	
 	//comment 1234
-	//branch test1112222222233333
+	//branch test11122222222333334444
 	static final String URL_UPLOAD_FILE = "http://localhost:8080/api/v1/logEncode";
 
 	  private org.slf4j.Logger logger = LoggerFactory.getLogger(FileResourcesController.class);
